03215b5957ddcb4ba69160d876def936b58f7b1e
alexandrebouayad <dr.alexandre.bouayad@gmail.com>
2021-12-17 20:05:51 +0100

    Update model

--- mnist.py
+++ mnist.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt
@@ -5,45 +7,53 @@ from tensorflow import keras
 
 from model import GAN
 
+IMAGE_DIR = Path("images")
+
 IMG_ROWS = 28
 IMG_COLUMNS = 28
 IMG_CHANNELS = 1
+
 BATCH_SIZE = 64
 LATENT_DIM = 128
 LEARNING_RATE = 0.0003
-EPOCHS = 1
+EPOCHS = 400
 
 
-class PNGLogger(keras.callbacks.Callback):
+class ImageLogger(keras.callbacks.Callback):
     def on_epoch_begin(self, epoch, logs=None):
         latent_dim = self.model.latent_dim
-        noise = tf.random.normal((100, latent_dim))
+        noise = tf.random.normal((25, latent_dim))
         generated_images = self.model(noise)
+        generated_images = tf.squeeze(generated_images, axis=-1)
 
-        plt.figure(figsize=(10, 10))
-        for i, image in enumerate(generated_images):
-            plt.subplot(10, 10, i + 1)
-            plt.imshow(image, cmap="gray", vmin=0, vmax=1)
-            plt.axis("off")
-        plt.tight_layout()
-        plt.savefig(f"{epoch}.png")
+        with plt.ioff():
+            plt.figure(figsize=(5, 5))
+            for idx, image in enumerate(generated_images, start=1):
+                plt.subplot(5, 5, idx)
+                plt.imshow(image, cmap="gray", vmin=-1, vmax=1)
+                plt.axis("off")
+            plt.tight_layout()
+            plt.savefig(IMAGE_DIR / f"{epoch}.png")
+            plt.close()
 
 
 (x_train, _), (x_test, _) = keras.datasets.mnist.load_data()
 all_digits = np.concatenate([x_train, x_test])
-all_digits = all_digits.astype("float32") / 127.5 - 1
-all_digits = np.reshape(all_digits, (-1, 28, 28, 1))
+all_digits = all_digits.astype("float32") / 127.5 - 1.0
+all_digits = np.expand_dims(all_digits, axis=-1)
 dataset = tf.data.Dataset.from_tensor_slices(all_digits)
-dataset = dataset.shuffle(buffer_size=1024).batch(BATCH_SIZE)
+buffer_size = len(dataset)
+dataset = dataset.shuffle(buffer_size).batch(BATCH_SIZE)
 
 gan = GAN(IMG_ROWS, IMG_COLUMNS, IMG_CHANNELS, LATENT_DIM)
 gan.compile(
     d_optimizer=keras.optimizers.Adam(LEARNING_RATE),
     g_optimizer=keras.optimizers.Adam(LEARNING_RATE),
     loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=["accuracy"],
 )
 
 
 if __name__ == "__main__":
-    png_logger = PNGLogger()
-    gan.fit(dataset, epochs=EPOCHS, callbacks=[png_logger])
+    image_logger = ImageLogger()
+    gan.fit(dataset, epochs=2, steps_per_epoch=1000, callbacks=[image_logger])
--- model.py
+++ model.py
@@ -10,27 +10,17 @@ class GAN(Model):
         self.latent_dim = latent_dim
         self.discriminator = self._build_discriminator()
         self.generator = self._build_generator()
+        # self.discriminator.trainable = False
         self.combined = Sequential([self.generator, self.discriminator])
 
     def _build_discriminator(self):
         return Sequential(
             [
-                # layers.InputLayer(self.img_shape),
-                layers.Conv2D(
-                    filters=64,
-                    kernel_size=(3, 3),
-                    strides=(2, 2),
-                    padding="same",
-                ),
+                layers.Flatten(input_shape=self.img_shape),
+                layers.Dense(units=512),
                 layers.LeakyReLU(alpha=0.2),
-                layers.Conv2D(
-                    filters=128,
-                    kernel_size=(3, 3),
-                    strides=(2, 2),
-                    padding="same",
-                ),
+                layers.Dense(units=256),
                 layers.LeakyReLU(alpha=0.2),
-                layers.GlobalMaxPool2D(),
                 layers.Dense(units=1),
             ]
         )
@@ -38,40 +28,27 @@ class GAN(Model):
     def _build_generator(self):
         return Sequential(
             [
-                # layers.InputLayer((self.latent_dim,)),
-                layers.Dense(7 * 7 * 128),
+                layers.Dense(units=256, input_dim=self.latent_dim),
                 layers.LeakyReLU(alpha=0.2),
-                layers.Reshape((7, 7, 128)),
-                layers.Conv2DTranspose(
-                    filters=128,
-                    kernel_size=(4, 4),
-                    strides=(2, 2),
-                    padding="same",
-                ),
+                layers.BatchNormalization(momentum=0.8),
+                layers.Dense(units=512),
                 layers.LeakyReLU(alpha=0.2),
-                layers.Conv2DTranspose(
-                    filters=128,
-                    kernel_size=(4, 4),
-                    strides=(2, 2),
-                    padding="same",
-                ),
+                layers.BatchNormalization(momentum=0.8),
+                layers.Dense(units=1024),
                 layers.LeakyReLU(alpha=0.2),
-                layers.Conv2D(
-                    filters=1,
-                    kernel_size=(7, 7),
-                    padding="same",
-                    activation="sigmoid",
-                ),
+                layers.BatchNormalization(momentum=0.8),
+                layers.Dense(np.prod(self.img_shape), activation="tanh"),
+                layers.Reshape(self.img_shape),
             ]
         )
 
     def call(self, inputs):
         return self.generator(inputs)
 
-    def compile(self, d_optimizer, g_optimizer, loss):
+    def compile(self, d_optimizer, g_optimizer, loss, metrics):
         super().compile()
-        self.discriminator.compile(d_optimizer, loss)
-        self.combined.compile(g_optimizer, loss)
+        self.discriminator.compile(d_optimizer, loss, metrics)
+        self.combined.compile(g_optimizer, loss, metrics)
 
     def train_step(self, real_images):
         if isinstance(real_images, tuple):
@@ -79,20 +56,21 @@ class GAN(Model):
         batch_size = tf.shape(real_images)[0]
         latent_shape = batch_size, self.latent_dim
 
+        ones = tf.ones((batch_size, 1))
+        zeros = tf.zeros((batch_size, 1))
+        ones_zeros = tf.concat([ones, zeros], axis=0)
+
         noise = tf.random.normal(latent_shape)
         generated_images = self.generator(noise)
         images = tf.concat([real_images, generated_images], axis=0)
-        zeros = tf.zeros((batch_size, 1))
-        ones = tf.ones((batch_size, 1))
-        labels = tf.concat([zeros, ones], axis=0)
-        d_loss = self.discriminator.train_step((images, labels))
-        d_loss["d_loss"] = d_loss.pop("loss")
+        d_results = self.discriminator.train_step((images, ones_zeros))
 
         noise = tf.random.normal(latent_shape)
-        labels = tf.zeros((batch_size, 1))
         self.discriminator.trainable = False
-        g_loss = self.combined.train_step((noise, labels))
+        g_results = self.combined.train_step((noise, ones))
         self.discriminator.trainable = True
-        g_loss["g_loss"] = g_loss.pop("loss")
 
-        return d_loss | g_loss
+        return {
+            **{f"d_{key}": value for key, value in d_results.items()},
+            **{f"g_{key}": value for key, value in g_results.items()},
+        }
